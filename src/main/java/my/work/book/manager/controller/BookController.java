3ed567e60b8e5d37f6e8859a7eaa27fbd7e31c83
george dinu <george.dinu.ro@gmail.com>
2026-07-15 22:38:13 +0300

    add POST create Book method inside controller

--- src/main/java/my/work/book/manager/controller/BookController.java
+++ src/main/java/my/work/book/manager/controller/BookController.java
@@ -6,6 +6,8 @@ import my.work.book.manager.service.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,11 @@ public class BookController {
         return bookService.findByTitle(title)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    ResponseEntity<Book> create(@RequestBody Book book) {
+        return null;
     }
 
 }
