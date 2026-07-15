af74846fd27006dc76cf10970de38f4f52a44bba
george dinu <george.dinu.ro@gmail.com>
2026-07-15 08:16:10 +0300

    initial commit

---
+++ src/main/java/my/work/book/manager/BookManagerUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package my.work.book.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerUsingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagerUsingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/work/book/manager/BookManagerUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package my.work.book.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerUsingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
