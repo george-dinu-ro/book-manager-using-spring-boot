879d20f2deed0e9d6f9c242237b7c4cf6ac28d12
george dinu <george.dinu.ro@gmail.com>
2026-07-15 08:56:41 +0300

    add Book controller

---
+++ src/main/java/my/work/book/manager/controller/BookController.java
@@ -0,0 +1,14 @@
+package my.work.book.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/book")
+@RestController
+public class BookController {
+
+
+
+
+
+}
