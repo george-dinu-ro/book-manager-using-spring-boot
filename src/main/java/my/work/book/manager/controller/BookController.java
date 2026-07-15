3c1f525fe160225baf0fe25e0f9325fa0535045a
george dinu <george.dinu.ro@gmail.com>
2026-07-15 09:01:25 +0300

    add read() method inside Book controller

--- src/main/java/my/work/book/manager/controller/BookController.java
+++ src/main/java/my/work/book/manager/controller/BookController.java
@@ -1,14 +1,20 @@
 package my.work.book.manager.controller;
 
+import my.work.book.manager.entity.Book;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/book")
+import java.util.Collections;
+import java.util.List;
+
+@RequestMapping("/api/v1/books")
 @RestController
 public class BookController {
 
-
-
-
+    @GetMapping
+    List<Book> read() {
+        return Collections.emptyList();
+    }
 
 }
