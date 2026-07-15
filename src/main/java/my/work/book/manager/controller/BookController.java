2dc37ecdf004efea226d6036ee8fa8c5e471a2b6
george dinu <george.dinu.ro@gmail.com>
2026-07-15 09:03:21 +0300

    add read() method inside Book service

--- src/main/java/my/work/book/manager/controller/BookController.java
+++ src/main/java/my/work/book/manager/controller/BookController.java
@@ -1,20 +1,24 @@
 package my.work.book.manager.controller;
 
+import lombok.RequiredArgsConstructor;
 import my.work.book.manager.entity.Book;
+import my.work.book.manager.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RequestMapping("/api/v1/books")
 @RestController
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
 
     @GetMapping
     List<Book> read() {
-        return Collections.emptyList();
+        return bookService.read();
     }
 
 }
--- src/main/java/my/work/book/manager/service/BookService.java
+++ src/main/java/my/work/book/manager/service/BookService.java
@@ -1,7 +1,16 @@
 package my.work.book.manager.service;
 
+import my.work.book.manager.entity.Book;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class BookService {
+
+    public List<Book> read() {
+        return Collections.emptyList();
+    }
+
 }
