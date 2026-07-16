a2fc8b28f7e13ae2af0dd5487f102f4cc723e1bb
george dinu <george.dinu.ro@gmail.com>
2026-07-16 16:33:21 +0300

    add validation to BookRequest dto

--- src/main/java/my/work/book/manager/request/BookRequest.java
+++ src/main/java/my/work/book/manager/request/BookRequest.java
@@ -1,7 +1,22 @@
 package my.work.book.manager.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
-public record BookRequest(String title, String author, String category, int rating) {
+public record BookRequest(
+        @Size(min = 1, max = 50)
+        String title,
+
+        @Size(min = 1, max = 50)
+        String author,
+
+        @Size(min = 1, max = 50)
+        String category,
+
+        @Min(1)
+        @Max(5)
+        int rating) {
 }
